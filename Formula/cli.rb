8daacabf5286f1307efff86d8cd67a63b24a4091
Mike Brown <github@torvosoft.com>
2022-02-22 23:31:54 +0000

    It works
    Add description, homepage, license

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,9 +1,9 @@
 class Cli < Formula
-  desc ""
-  homepage ""
+  desc "Bopmatic CLI"
+  homepage "github.com/bopmatic/cli"
   url "https://github.com/bopmatic/cli/archive/refs/tags/v0.9.3.tar.gz"
   sha256 "cec4a045862b9ba544931d9bca52323ed6a2194b7e8e710b8fa69f89e2f4a129"
-  license ""
+  license "AGPL-3.0-only"
 
   depends_on "go" => :build
 
