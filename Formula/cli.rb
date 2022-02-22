141da251a331c6af71d187f3f539aafa2175bcb4
Mike Brown <github@torvosoft.com>
2022-02-22 22:27:31 +0000

    Initial attempt at homebrew tap & formula
    Created with:
    
      $ brew tap-new bopmatic/macos
      $ brew create --go --tap bopmatic/macos --set-name cli https://github.com/bopmatic/cli/archive/refs/tags/v0.9.1.tar.gz

---
+++ Formula/cli.rb
@@ -0,0 +1,18 @@
+class Cli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bopmatic/cli/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "d99112d52b70c003410ff23f5473c41d66601c5ad9a3c76bc049f2f78293612f"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    ENV.deparallelize
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "bopmatic", "version"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# homebrew-macos
-Homebrew tap for Bopmatic
+# Bopmatic Macos
+
+## How do I install these formulae?
+
+`brew install bopmatic/macos/<formula>`
+
+Or `brew tap bopmatic/macos` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
